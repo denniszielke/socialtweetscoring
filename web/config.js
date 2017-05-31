2919d23b4aed1f421b4b12e26b20e1b29b3de8d9
Dennis Zielke <dzielke@microsoft.com>
2017-05-31 16:19:53 +0200

    adde deployment file

--- web/config.js
+++ web/config.js
@@ -4,6 +4,6 @@ config.host = process.env.ENDPOINT || "[URI for DocumentDB endpoint]";
 config.authKey = process.env.AUTHKEY || "[Master key for DocumentDB]";
 config.instrumentationKey = process.env.INSTRUMENTATIONKEY;
 config.databaseId = "social";
-config.collectionId = "goodtweets";
+config.collectionId = "tweets";
 
 module.exports = config;
